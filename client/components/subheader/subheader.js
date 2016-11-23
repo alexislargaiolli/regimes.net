99e846e49884c7e73bf7b01073fba16434629830
alex <alexis.largaiolli@gmail.com>
2016-11-23 20:25:28 +0100

    Ajout d'un sous menu dans le subheader pour les regimes

--- client/components/subheader/subheader.js
+++ client/components/subheader/subheader.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('regimesApp')
-    .controller('SubheaderCtrl', function($rootScope, $scope, $state, Diet) {
+    .controller('SubheaderCtrl', function($rootScope, $window, $scope, $state, Diet) {
         $scope.currentState = $state.current;
 
         $scope.healthDiets = [];
@@ -14,5 +14,35 @@ angular.module('regimesApp')
         });
 
         $scope.vi = true;
+
+        $scope.onLinkClicked = function(dietId) {
+            $state.go('diet', { id: dietId });
+        }
+
+        $scope.showMenu = function(id) {
+            angular.element(document.querySelector('#submenuSlimmingDiet')).removeClass('visible');
+            angular.element(document.querySelector('#submenuHealthDiet')).removeClass('visible');
+            var menu = angular.element(document.querySelector(id));
+            menu.addClass('visible');
+        }
+
+        $scope.hideMenu = function(id) {
+            var menu = angular.element(document.querySelector(id));
+            menu.removeClass('visible');
+        }
+
+        function updateSubMenus() {
+            console.log('updateSubMenus');
+            var width = document.querySelector('#subheaderSlimmingBtn').clientWidth;
+            angular.element(document.querySelector('#submenuSlimmingDiet')).css('width', width + 'px');
+            angular.element(document.querySelector('#submenuHealthDiet')).css('width', width + 'px');
+        }
+
+        angular.element($window).bind('resize', function() {
+            updateSubMenus();
+        });
+        angular.element(document).ready(function() {
+            updateSubMenus();
+        });
 
     });
