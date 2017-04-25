1111f48678398346d744675eb531a4f92f3b4250
Alexis Largaiolli <alexis.largaiolli@gmail.com>
2017-04-25 17:02:19 +0200

    Fix version + fix compilation error + fix  bugs

--- client/app/app.js
+++ client/app/app.js
@@ -38,7 +38,7 @@ angular.module('regimesApp', [
     .config(function(uiGmapGoogleMapApiProvider) {
         uiGmapGoogleMapApiProvider.configure({
             key: 'AIzaSyDqLwxyqZckv23WJ_JwOCWCg65-5Blp6ok',
-            v: '3.20', //defaults to latest 3.X anyhow
+            v: '3.26', //defaults to latest 3.X anyhow
             libraries: 'geometry,visualization,places',
         });
     })
